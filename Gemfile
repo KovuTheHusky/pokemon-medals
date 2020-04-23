9d7b38a1d0d6a0b94415640805d76d7627e867d8
KovuTheHusky <kovuthehusky@gmail.com>
2020-04-23 14:58:13 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+gem 'fileutils'
+gem 'git'
+gem 'i18n'
+gem 'json'
+gem 'rmagick'
---
+++ generate.rb
@@ -0,0 +1,86 @@
+require 'fileutils'
+require 'git'
+require 'i18n'
+require 'json'
+require 'rmagick'
+
+include Magick
+
+I18n.available_locales = [:en]
+
+colors = ['shadow', 'bronze', 'silver', 'gold']
+dir = 'assets/decrypted_assets/png/'
+empty = Image.new(256, 256) {
+  self.background_color = 'transparent'
+}
+specials = [
+  ['5001', '5021', '5031'],
+  ['5039'],
+  ['5100']
+]
+
+FileUtils.rm_rf('assets')
+Git.clone('https://github.com/ZeChrales/PogoAssets.git', 'assets')
+
+colors.each do |color|
+  FileUtils.mkdir_p('standard/' + color)
+  FileUtils.mkdir_p('type/' + color)
+end
+FileUtils.mkdir_p('special');
+
+# Standard
+
+JSON.parse(File.read('standard.json')).each do |entry|
+  name = I18n.transliterate(entry[1].tr(' ', '-').downcase)
+  for i in 1..3 do
+    inner = ImageList.new(dir + 'Badge_' + entry[0] + '_' + i.to_s + '_01.png').scale(144, 144)
+    outer = ImageList.new(dir + 'badge_ring_' + i.to_s + '.png')
+    icon = outer.composite(inner, CenterGravity, OverCompositeOp)
+    icon.write('standard/' + colors[i] + '/' + name + '.png')
+  end
+  shadow = ImageList.new(dir + 'Badge_' + entry[0] + '_1_01.png').scale(144, 144)
+  shadow.alpha(ExtractAlphaChannel)
+  shadow.fuzz = '50%'
+  shadow = shadow.negate().transparent('white').opaque('black', '#efefef').blur_image(0, 0.5)
+  icon = empty.composite(shadow, CenterGravity, OverCompositeOp)
+  icon.write('standard/' + colors[0] + '/' + name + '.png')
+end
+
+# Type
+
+JSON.parse(File.read('type.json')).each do |entry|
+  name = I18n.transliterate(entry[1].tr(' ', '-').downcase)
+  inner = ImageList.new(dir + 'Badge_' + entry[0] + '_01.png').scale(144, 144)
+  for i in 1..3 do
+    outer = ImageList.new(dir + 'badge_ring_' + i.to_s + '.png')
+    icon = outer.composite(inner, CenterGravity, OverCompositeOp)
+    icon.write('type/' + colors[i] + '/' + name + '.png')
+  end
+  shadow = inner
+  shadow.alpha(ExtractAlphaChannel)
+  shadow.fuzz = '50%'
+  shadow = shadow.negate().transparent('white').opaque('black', '#efefef').blur_image(0, 0.5)
+  icon = empty.composite(shadow, CenterGravity, OverCompositeOp)
+  icon.write('type/' + colors[0] + '/' + name + '.png')
+end
+
+# Special
+
+JSON.parse(File.read('special.json')).each do |entry|
+  name = I18n.transliterate(entry[1].tr(' ', '-').downcase)
+  inner = ImageList.new(dir + 'Badge_' + entry[0] + '_01.png')
+  outer = nil
+  for i in 0..2 do
+    if specials[i].include? entry[0]
+      outer = ImageList.new(dir + 'badge_frame_' + i.to_s + '.png')
+    end
+  end
+  if outer.nil?
+    FileUtils.cp(dir + 'Badge_' + entry[0] + '_01.png', 'special/' + name + '.png')
+  else
+    icon = outer.composite(inner, CenterGravity, OverCompositeOp)
+    icon.write('special/' + name + '.png')
+  end
+end
+
+FileUtils.rm_rf('assets')
