bd06e043b052fa5fe691208b8ed37d7b78e6f32d
KovuTheHusky <kovuthehusky@gmail.com>
2020-05-05 21:34:49 +0000

    Travis #4

--- optimize.sh
+++ optimize.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . -type f -name "*.png" -exec pngquant --force --skip-if-larger --output {} --speed 1 --strip --verbose {} \;
+find . -type f -name "*.png" -exec pngquant --force --skip-if-larger --output {} --speed 1 --strip {} \;
