c2d686807d420a83467e275cae716fabd7e07c6e
KovuTheHusky <kovuthehusky@gmail.com>
2020-05-11 05:06:21 -0400

    Don't bother with git user and email

--- deploy.sh
+++ deploy.sh
@@ -16,8 +16,6 @@ for i in {1..10}; do find . -type f -name "*.png" -exec pngquant --force --skip-
 bundle exec jekyll build
 
 cd _site
-git config user.email "kovuthehusky@gmail.com"
-git config user.name "KovuTheHusky"
 git add --all
 git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
 git push --force origin gh-pages
